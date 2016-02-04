fbb9683f55df2c2e375ea7f89a8e78ecc1565d32
MrChens <Mr_Chens@outlook.com>
2016-02-04 18:17:27 +0800

    Initial Commit

---
+++ Widget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Widget
+//
+//  Created by MrChens on 16/2/4.
+//  Copyright © 2016年 chinanetcenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Widget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Widget
+//
+//  Created by MrChens on 16/2/4.
+//  Copyright © 2016年 chinanetcenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
